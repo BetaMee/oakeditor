61b8ca21270df9d3ef89dc3ed176020e117d0564
betamee <gongxq95@gmail.com>
2018-04-07 13:22:12 +0800

    update IconWrapper

--- src/components/common/components/IconWrapper.jsx
+++ src/components/common/components/IconWrapper.jsx
@@ -1,36 +1,38 @@
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
 
-const hover = ({ isAllowed }) => isAllowed && `
+const hover = ({ isAllowed, hoverColor }) => isAllowed && `
   &:hover {
-    color: rgba(255, 255, 255, 1);
+    color: ${hoverColor};
     background-color: hsla(0,0%,100%,.1);
   }
 `
 
 const IconWrapper = styled.span`
-  width: ${({ wWidth }) => `${wWidth}px`};
-  height: ${({ wHeight }) => `${wHeight}px`};
+  width: ${({ wSize }) => `${wSize}px`};
+  height: ${({ wSize }) => `${wSize}px`};
   display: flex;
   flex-direction: row;
   justify-content: center;
   align-items: center;
   cursor: ${({ isAllowed }) => isAllowed ? 'pointer' : 'not-allowed'};
   border-radius: 4px;
-  color: #b9b9b9;
+  color: ${({ wColor }) => wColor};
   ${hover}
 `
 IconWrapper.defaultProps = {
-  wWidth: 38,
-  wHeight: 38,
+  wSize: 38,
   isAllowed: true,
+  wColor: '#b9b9b9',
+  hoverColor: 'rgba(255, 255, 255, 1)',
 }
 
 IconWrapper.propTypes = {
   children: PropTypes.element.isRequired,
-  wWidth: PropTypes.number.isRequired,
-  wHeight: PropTypes.number.isRequired,
+  wSize: PropTypes.number.isRequired,
   isAllowed: PropTypes.bool.isRequired,
+  wColor: PropTypes.string.isRequired,
+  hoverColor: PropTypes.string.isRequired,
 }
 
 export default IconWrapper
