a9e59099b70008a8284f26ce3a5299f7084cdb5c
betamee <gongxq95@gmail.com>
2018-04-07 14:47:25 +0800

    update svg icon

--- src/components/common/icons/HorizontalRule.jsx
+++ src/components/common/icons/HorizontalRule.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const HorizontalRule = ({ className }) =>
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className={className}>
-    <path fill-opacity="1" stroke-linejoin="round" d="M3,13l5,0l0,-2l-5,0l0,2Zm6.5,0l5,0l0,-2l-5,0l0,2Zm6.5,0l5,0l0,-2l-5,0l0,2Z">
+    <path fillOpacity="1" strokeLinejoin="round" d="M3,13l5,0l0,-2l-5,0l0,2Zm6.5,0l5,0l0,-2l-5,0l0,2Zm6.5,0l5,0l0,-2l-5,0l0,2Z">
     </path>
   </svg>
 
