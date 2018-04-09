6feb5829f2f73aa869ac047db0954aada1e569d8
betamee <gongxq95@gmail.com>
2018-04-09 11:42:09 +0800

    add Toast

---
+++ src/components/common/components/Toast.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import Modal from './Modal'
+
+class Toast extends Component {
+  state = {
+
+  }
+
+  render() {
+    return (
+      <Modal></Modal>
+    )
+  }
+}
+
+export default Toast
