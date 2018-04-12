94a9b4a2b50a88fbb34689cc8afbe33f35491328
betamee <gongxq95@gmail.com>
2018-04-12 12:24:10 +0800

    add assetspanel

---
+++ src/components/assetspanel/Panel.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+import Wrapper from '../common/components/Wrapper'
+
+
+class Panel extends Component {
+  state = {
+
+  }
+  render() {
+    return (
+    <Wrapper>
+      Hello
+    </Wrapper>
+    )
+  }
+}
+
+export default Panel
--- src/components/assetspanel/index.jsx
+++ src/components/assetspanel/index.jsx
@@ -1,13 +1,35 @@
 import React, { Component } from 'react'
 
+import Wrapper from '../common/components/Wrapper'
+// Modal组件
+import Modal from '../common/components/Modal'
+// 组件
+import Panel from './Panel'
+
+const AssetsWrapper = Wrapper.extend`
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 999;
+  background-color: rgba(0,0,0,0.3);
+`
+
 class AssetsPanel extends Component {
   render() {
+    const { hideAssetsPanel } = this.props
     return (
-      <div>
-        AssetsPanel
-      </div>
+      <Modal>
+        <AssetsWrapper
+          onClick={hideAssetsPanel}
+          isUserSelect={false}
+        >
+          {/* Assets panel */}
+          <Panel />
+        </AssetsWrapper>
+      </Modal>
     )
   }
 }
 
 export default AssetsPanel
+
