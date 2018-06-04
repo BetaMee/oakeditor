f05917d7e42c9daa4e46922891ae27d5830f0e4b
betamee <gongxq95@gmail.com>
2018-06-05 02:42:21 +0800

    remove warn

--- src/components/filepanel/PanelTitle.jsx
+++ src/components/filepanel/PanelTitle.jsx
@@ -1,18 +1,7 @@
 import React from 'react'
 
 import Wrapper from '../common/components/Wrapper'
-import SVGIconWrapper from '../common/components/SVGIconWrapper'
-import SVGIcon from '../common/components/SVGIcon'
 
-const TitleSVGIconWrapper = SVGIconWrapper.extend`
-  &:hover {
-    background-color: rgba(0,0,0,.1);
-    opacity: 1;
-    fill: #333;
-  }
-  fill: #333;
-  opacity: .75;
-`
 
 const PanelTitle = () =>
   <Wrapper
